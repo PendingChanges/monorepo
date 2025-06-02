f8e7c23cfe17edbd14628b9493d8f5e448a7a694
Rémi Aubert <aubert.remi@gmail.com>
2025-06-02 15:30:17 +0200

    :wrench: add bruno collection files

--- back/EmailTest/test.cs
+++ back/EmailTest/test.cs
@@ -1,14 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MailKit;
+using MailKit.Search;
 
-namespace EmailTest;
-internal class Test
+using MailKit.Net.Imap;
+
+namespace TestClient;
+
+static class Program
 {
-    public void Test()
+    public static void Main(string[] args)
     {
+        using (var client = new ImapClient())
+        {
+            using (var cancel = new CancellationTokenSource())
+            {
+                client.Connect("imap.gmail.com", 993, true, cancel.Token);
 
+                // If you want to disable an authentication mechanism,
+                // you can do so by removing the mechanism like this:
+                client.AuthenticationMechanisms.Remove("XOAUTH");
+
+                client.Authenticate("aubert.remi", "irfw txml ceqf vcna", cancel.Token);
+
+                // The Inbox folder is always available...
+                var inbox = client.Inbox;
+                inbox.Open(FolderAccess.ReadOnly, cancel.Token);
+
+                Console.WriteLine("Total messages: {0}", inbox.Count);
+                Console.WriteLine("Recent messages: {0}", inbox.Recent);
+
+                // download each message based on the message index
+                for (int i = 0; i < inbox.Count; i++)
+                {
+                    var message = inbox.GetMessage(i, cancel.Token);
+                    Console.WriteLine("Subject: {0}", message.Subject);
+                }
+
+                // let's try searching for some messages...
+                var query = SearchQuery.All;
+
+                foreach (var uid in inbox.Search(query, cancel.Token))
+                {
+                    var message = inbox.GetMessage(uid, cancel.Token);
+                    Console.WriteLine("[match] {0}: {1}", uid, message.Subject);
+                }
+
+                client.Disconnect(true, cancel.Token);
+            }
+        }
     }
 }
