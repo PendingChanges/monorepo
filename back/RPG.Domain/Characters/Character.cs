f402fb3d3c7cf3e6409387be3901eb6953d8cae8
Rémi Aubert <aubert.remi@gmail.com>
2025-09-17 18:41:39 +0200

    upgrade packages

--- back/RPG.Domain/Characters/Character.cs
+++ back/RPG.Domain/Characters/Character.cs
@@ -1,14 +1,27 @@
 ﻿
 namespace RPG.Domain.Characters;
 
-public record Character(string Name, ExperienceTable ExperienceTable, int ExperiencePoints = 0)
+public record Character
 {
-    public Character? Rename(string newName) 
+    public Character(string name, ExperienceTable experienceTable, int experiencePoints)
+    {
+        Name = name;
+        ExperienceTable = experienceTable;
+        ExperiencePoints = experiencePoints;
+    }
+
+    public Character(string name, ExperienceTable experienceTable) : this(name, experienceTable, 0) { }
+
+    public string Name { get; private set; }
+    public ExperienceTable ExperienceTable { get; private set; }
+    public int ExperiencePoints { get; private set; }
+
+    public Character? Rename(string newName)
         => this with { Name = newName };
 
-    public Character GainExperience(int points) 
+    public Character GainExperience(int points)
         => this with { ExperiencePoints = ExperiencePoints + points };
 
-    public int GetLevel() 
+    public int GetLevel()
         => ExperienceTable.GetLevel(ExperiencePoints);
 }
